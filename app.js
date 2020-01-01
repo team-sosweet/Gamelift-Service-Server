1207859b42f96ab8c72adc0c23deab3f3ca1eb7d
huewilliams <4879sinwoo@naver.com>
2020-01-01 15:29:04 +0900

    ADD: server and dependencies

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+let cors = require('cors');
+let Err = require('./utils/Err');
+
+let indexRouter = require('./routes/index');
+let app = express();
+
+app.set('port', 3000);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+    next(new Err(404, "Not Found"));
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        description: err.message || 'unknown internal server error.'
+    })
+})
+
+app.listen(app.get('port'), ()=> {
+    console.log(`server is listening on ${app.get('port')} port.`);
+})
