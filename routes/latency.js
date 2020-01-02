58e5cb54a30ef36da42870b90b34cbc72100f59d
huewilliams <4879sinwoo@naver.com>
2020-01-02 10:59:07 +0900

    FIX: latency API response 수정

--- routes/latency.js
+++ routes/latency.js
@@ -9,13 +9,14 @@ router.get('/start', function(req, res, next) {
     let id = req.query.id;
     let time = new Date();
     latencies[id] = time;
-    res.status(200).json(latencies);
+    res.status(200).json({description: 'latency checking started.'});
 })
 
 router.get('/end', function(req, res, next) {
     let id = req.query.id;
     let time = new Date();
     let result = time - latencies[id];
+    delete latencies[id];
     res.status(200).json({latency: result});
 })
 
