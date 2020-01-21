3cf680170c86e6f632c77fa8e614f1ffb6cbb527
huewilliams <4879sinwoo@naver.com>
2020-01-22 01:21:23 +0900

    ADD: batch 작업(특정 시간에 game fleet 실행 및 중지)

--- routes/fleet.js
+++ routes/fleet.js
@@ -1,29 +1,55 @@
 const express = require('express');
 const schedule = require('node-schedule');
 
+let gamelift = require('../utils/aws');
 const cache = require('../utils/redisClient');
 
 let router = express.Router();
 
-router.post('/id', async function(req, res, next) {
+router.post('/id', function(req, res, next) {
     let fleetId = req.body.fleetId;
     cache.set('fleetId', fleetId, function(err, data) {
-        res.json(data);
+        res.status(201).json({description: data});
     });
 })
 
 router.get('/', async function(req, res, next) {
     cache.get('fleetId', function(err, data) {
-        res.json(data);
+        res.json({description: data});
     })
 })
 
-let scheduler = schedule.scheduleJob({hour: 23, minute: 48}, function() {
-    console.log('hello');
+let startGame = schedule.scheduleJob({hour: 17, minute: 25}, async function() {
+    await updateFleetCapacity(1);
 })
 
-// function updateFleetCapacity() {
-    
-// }
+let endGame = schedule.scheduleJob({hour: 18, minute: 40}, async function() {
+    await updateFleetCapacity(0);
+})
+
+
+async function updateFleetCapacity(size) {
+    let fleetId = await getFleetId();
+    let updateParams = {
+        FleetId: fleetId,
+        DesiredInstances: size
+    }
+
+    return new Promise(function(resolve, reject) {
+        gamelift.updateFleetCapacity(updateParams, function(err, data) {
+            if (err) reject(err);
+            else resolve(data);
+        })
+    })
+}
+
+function getFleetId() {
+    return new Promise(function(resolve, reject) {
+        cache.get('fleetId', function(err, data) {
+            if(err) reject(err);
+            else resolve(data);
+        })
+    })
+}
 
 module.exports = router;
